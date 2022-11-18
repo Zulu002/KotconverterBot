4bb526f37e46a7a534616e05e2d5f9128a9949d1
Egor Lyadskiy <oraproavp@list.ru>
2022-11-18 18:59:09 +0300

    upload
    upload

--- BotHakaton/Bot_1/Bot.py
+++ BotHakaton/Bot_1/Bot.py
@@ -1,5 +1,6 @@
 import telebot
 from telebot import types
+import requests
 
 dokbot = telebot.TeleBot("5823350388:AAFQs-96R4kWQ7hDhDTc4Rtcuwmxb2BHKlE")
 
@@ -11,6 +12,19 @@ def start(message):
     button_3 = types.KeyboardButton('Txt')
     markup.add(button_1, button_2, button_3)
     dokbot.send_message(message.chat.id, f'Привет {message.from_user.first_name}, это бот, который конвертирует файл формата frx в выбранный тобою формат.', reply_markup=markup)
+
+@dokbot.message_handler(content_types=['text'])
+def message_user(message):
+    get_message = message.text.strip().lower()
+    if get_message == 'docx':
+        dokbot.send_message(message.chat.id, "s" )
+    elif get_message == 'txt':
+        dokbot.send_message(message.chat.id, 'e')
+    elif get_message == "pdf":
+        dokbot.send_message(message.chat.id, 'rop')
+    else:
+        dokbot.send_message(message.chat.id, 'Ошибка')
+
 
 
 dokbot.polling(none_stop=True)
