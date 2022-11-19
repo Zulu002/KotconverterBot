2325a2444e18cc3699d83952a9684b9a3506a37b
Egor Lyadskiy <oraproavp@list.ru>
2022-11-19 11:52:23 +0300

    upload
    upload

--- BotHakaton/Bot_1/converter.py
+++ BotHakaton/Bot_1/converter.py
@@ -1,4 +1,5 @@
-import convertapi
+import requests
+api_key = "048572a9a42e7b7a0574d1487ad5a059"
+response = requests.get("http://api.convertio.co/convert", )
+print(response)
 
-def convert(files):
-    return files
